097dd49ef25109f32b78b9bf5a5f8d1c2fc29796
QAsmirnov <smirnov.kz1@yandex.ru>
2025-03-04 23:59:44 +0500

    test

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -4,32 +4,18 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
 
-
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    registration_link = page.get_by_test_id("login-page-registration-link")
-    registration_link.click()
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
+    expect(email_input).to_be_visible()
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    expect(registration_email_input).to_be_visible()
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
+    expect(password_input).to_be_visible()
 
-    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    expect(registration_username_input).to_be_visible()
+    login_button = page.get_by_test_id('login-page-login-button')
+    expect(login_button).to_be_visible()
 
-    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    expect(registration_password_input).to_be_visible()
+    email_input.fill("user.name@gmail.com")
+    password_input.fill("password")
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_visible()
-
-    registration_email_input.fill("user.name@gmail.com")
-    registration_username_input.fill("username")
-    registration_password_input.fill("password")
-
-    registration_button.click()
-
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_have_text("Dashboard")
-
-
-
+    login_button.click()
