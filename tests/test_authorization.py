f233069467251f0a4d061f4dad34026d132eaec5
QAsmirnov <smirnov.kz1@yandex.ru>
2025-06-15 21:23:30 +0500

    Введение в pytest и Интеграция pytest и playwright

---
+++ tests/test_authorization.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_wrong_email_or_password_authorization():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+        email_input = page.get_by_test_id('login-form-email-input').locator('input')
+        expect(email_input).to_be_visible()
+
+        password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        expect(password_input).to_be_visible()
+
+        login_button = page.get_by_test_id('login-page-login-button')
+        expect(login_button).to_be_visible()
+
+        email_input.fill("user.name@gmail.com")
+        password_input.fill("password")
+
+        login_button.click()
+
+        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        expect(wrong_email_or_password_alert).to_be_visible()
+        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+        browser.close()
---
+++ tests/test_pytest.py
@@ -0,0 +1,18 @@
+def test_user_login():
+    print("Hello World!")
+
+
+class TestUserLogin:
+    def test_1(self):
+        print("test1")
+
+    def test_2(self):
+        print("test2")
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5
