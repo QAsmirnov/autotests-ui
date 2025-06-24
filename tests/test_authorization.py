4c2790b77ddac3cf543174f1988d8dd9de589dd0
QAsmirnov <smirnov.kz1@yandex.ru>
2025-06-24 22:05:21 +0500

    Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,7 +4,13 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize(('email', 'password'),
+                         [
+                             ("user.name@gmail.com", "password"),
+                             ("user.name@gmail.com", "  "),
+                             ("  ", "password")
+                         ])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
@@ -16,12 +22,11 @@ def test_wrong_email_or_password_authorization(chromium_page: Page):
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     expect(login_button).to_be_visible()
 
-    email_input.fill("user.name@gmail.com")
-    password_input.fill("password")
+    email_input.fill(email)
+    password_input.fill(password)
 
     login_button.click()
 
     wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
